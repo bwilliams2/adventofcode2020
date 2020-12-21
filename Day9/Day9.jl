c9d0f4e07df11b172a6a6238d87630066f76f02a
Bryce Williams <bwilliams2@gmail.com>
2020-12-21 06:38:56 -0600

    Day 9 solution)

--- Day9/Day9.jl
+++ Day9/Day9.jl
@@ -14,4 +14,21 @@ end
 
 part1Ans = input[i]
 
+# Part 2
 
+cumulative = cumsum(input[1:i])
+
+startPos = 0
+endPos = 0
+sums = [0]
+for n in range(1, i, step=1)
+    cumnsums = cumulative .- cumulative[n]
+    if part1Ans in cumnsums
+        startPos = n + 1
+        endPos = Int(findfirst(part1Ans .== cumnsums))
+        sums = cumnsums
+        break
+    end
+end
+
+part2Ans = maximum(input[startPos:endPos]) + minimum(input[startPos:endPos])
